933cdfb8926d6d8447a195f356156dbc6049f9b6
lojackson20 <lukeoj@byu.edu>
2025-01-22 15:29:48 -0700

    update phase 0

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -8,22 +8,38 @@ package chess;
  */
 public class ChessMove {
 
+    private ChessPiece[][] squares = new ChessPiece[8][8];
+    private ChessPosition startPosition;
+    private ChessPosition endPosition;
+    private ChessPiece.PieceType promotionPiece;
+
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
+
+        if (promotionPiece != null) {
+            squares[endPosition.getRow()][endPosition.getColumn()] = new ChessPiece(promotionPiece, true);
+        } else{
+            squares[endPosition.getRow()][endPosition.getColumn()] = squares[startPosition.getRow()][startPosition.getColumn()];
+            squares[startPosition.getRow()][startPosition.getColumn()] = null;
+        }
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return startPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+
+        return endPosition;
     }
 
     /**
@@ -33,6 +49,7 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+
+        return promotionPiece;
     }
 }
