8317f2b80ed603b9929aad2f3afdd9add6632117
lojackson20 <lukeoj@byu.edu>
2025-04-09 20:38:49 -0600

    phase 6 update

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,5 +1,65 @@
+//package server.websocket;
+//
+//public class WebSocketHandler {
+//    //
+//}
 package server.websocket;
 
+import com.google.gson.Gson;
+import dataaccess.DataAccess;
+import exception.ResponseException;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.Timer;
+
+
+@WebSocket
 public class WebSocketHandler {
-    //
+
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    // when i recieve a message what do i do? from (UserGameCommands)
+    public void onMessage(Session session, String message) throws IOException {
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        switch (command.type()) {
+            case CONNECT -> connect(command.visitorName(), session);
+            case MAKE_MOVE -> makeMove(action.visitorName());
+            case LEAVE -> leave();
+            case RESIGN -> resign();
+        }
+    }
+
+    private void connect(String authToken, Session session) throws IOException {
+        connections.add(visitorName, session);
+        var message = String.format("%s is in the shop", visitorName);
+        var notification = new Notification(Notification.Type.ARRIVAL, message);
+        connections.broadcast(visitorName, notification);
+    }
+
+    private void makeMove(String visitorName) throws IOException {
+        connections.remove(visitorName);
+        var message = String.format("%s left the shop", visitorName);
+        var notification = new Notification(Notification.Type.DEPARTURE, message);
+        connections.broadcast(visitorName, notification);
+    }
+
+    public void leave(String petName, String sound) throws ResponseException {
+        try {
+            var message = String.format("%s says %s", petName, sound);
+            var notification = new Notification(Notification.Type.NOISE, message);
+            connections.broadcast("", notification);
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    public void resign() {
+
+    }
 }
